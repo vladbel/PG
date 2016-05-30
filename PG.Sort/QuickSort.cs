4bda8fd4149a1fba228839c51059eae8d7231144
Vladimir Beliaev <vladbel@live.com>
2016-05-30 18:39:27 -0500

    Quick Sort implementation

--- PG.Sort/QuickSort.cs
+++ PG.Sort/QuickSort.cs
@@ -8,5 +8,23 @@ namespace PG.Sort
 {
     public class QuickSort
     {
+        public static void Sort (int[] array)
+        {
+            Sort(array, 0, array.Length- 1);
+        }
+
+        private static void Sort (int[] array, int start, int end)
+        {
+            if ( start == end )
+            {
+                // exit
+                return;
+            }
+            else 
+            {
+                var pivotPosition = start;
+                
+            }
+        }
     }
 }
