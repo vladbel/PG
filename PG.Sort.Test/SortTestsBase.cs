b5d27f3a281b6633ecc70bfeaa7536a6e91849b4
Vladimir Beliaev <vladbel@live.com>
2016-05-30 18:21:37 -0500

    PG.Sort

---
+++ PG.Sort.Test/SortTestsBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.Sort.Test
+{
+    [TestClass]
+    public abstract class SortTestsBase
+    {
+        protected Action<int[]> _sort;
+
+        [TestMethod]
+        public void SortArray_With_3_Elements()
+        {
+            var array = new int[] { 2, 3, 1 };
+            _sort(array);
+            Assert.IsTrue(array[0] == 1);
+            Assert.IsTrue(array[1] == 2);
+            Assert.IsTrue(array[2] == 3);
+        }
+
+        [TestMethod]
+        public void SortArray_With_1_Element()
+        {
+            var array = new int[] { 1 };
+            _sort(array);
+            Assert.IsTrue(array[0] == 1);
+        }
+    }
+}
---
+++ PG.Sort.Test/StandardLibSortTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PG.Sort;
+
+namespace PG.Sort.Test
+{
+    [TestClass]
+    public class StandardLibTest: SortTestsBase
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sort = StandardLibSort.Sort;
+        }
+    }
+}
