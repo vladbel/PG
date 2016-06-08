af3f0efb6fbe8fa79ba688fe32dc9533ea937492
Vladimir Beliaev <vladbel@live.com>
2016-06-08 08:21:05 -0500

    PG.BT

---
+++ PG.BT/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.BT
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+}
