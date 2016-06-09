5c60c0bd66d894c94aaefc53447f54c24f4e17f1
Vladimir Beliaev <vladbel@live.com>
2016-06-09 17:36:41 -0500

    PG.BT.Tree: initial implementation

---
+++ PG.BT/Tree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.BT
+{
+    public class Tree
+    {
+        // traverse the tree and find max depth
+        public static int MaxDepth (Node root)
+        {
+            return 0;
+        }
+    }
+}
