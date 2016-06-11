ff7ca3426af114ae6e5fe0cd5c62a79f7be0cb49
Vladimir Beliaev <vladbel@live.com>
2016-06-11 18:13:33 -0500

    PG.LL

---
+++ PG.LL.Tests/FindFromTailTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.LL.Tests
+{
+    [TestClass]
+    public class FindFromTailTests
+    {
+        [TestMethod]
+        public void Find_1st_In_ListOf3()
+        {
+            var list = new Node<int>()
+            {
+                Value = 0,
+                Next = new Node<int>()
+                {
+                    Value = 1,
+                    Next = new Node<int>()
+                    {
+                        Value = 2 ,
+                        Next = null
+                    }
+                }
+            };
+
+            var result = Node<int>.FindFromTail<int>(list, 1);
+
+            Assert.IsTrue(result.Value == 1);
+        }
+
+        [TestMethod]
+        public void Find_2nd_In_ListOf3()
+        {
+            var list = Node<int>.Build<int>(new int[] {0, 1, 2 });
+            var result = Node<int>.FindFromTail<int>(list, 2);
+            Assert.IsTrue(result.Value == 0);
+
+        }
+
+
+        [TestMethod]
+        public void Find_0_In_ListOf3()
+        {
+            var list = Node<int>.Build<int>(new int[] { 0, 1, 2 });
+            var result = Node<int>.FindFromTail<int>(list, 0);
+            Assert.IsTrue(result.Value == 2);
+
+        }
+
+        [TestMethod]
+        public void Find_3_In_ListOf5()
+        {
+            var list = Node<int>.Build<int>(new int[] { 0, 1, 2, 3, 4 });
+            var result = Node<int>.FindFromTail<int>(list, 3);
+            Assert.IsTrue(result.Value == 1);
+
+        }
+    }
+}
---
+++ PG.LL/Node.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.LL
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+
+
+        #region "Find n-th element from the end"
+        public static Node<T1> FindFromTail<T1>(Node<T1> head, int n)
+        {
+            var result = Node<T1>.FindFromTail<T1>(head, n, 0);
+            return result.Item1;
+        }
+        private static Tuple<Node<T1>, int> FindFromTail<T1>(Node<T1> currentNode,
+                                           int n,
+                                           int currentNodeNumber)
+        {
+
+            if (currentNode.Next == null)
+            {
+                // we reach the end
+
+                if (n == 0)
+                    return new Tuple<Node<T1>, int>(currentNode, currentNodeNumber);
+                else
+                {
+                    return new Tuple<Node<T1>, int>(null, currentNodeNumber); ;
+                }
+            }
+
+            var result = FindFromTail(currentNode.Next, n, currentNodeNumber + 1);
+
+            if (result.Item2 - currentNodeNumber == n)
+            {
+                return new Tuple<Node<T1>, int>(currentNode, result.Item2);
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+
+        #endregion
+
+        #region "BuildListFromArray"
+        
+        public static Node<T1> Build<T1> (IEnumerable<T1> items)
+        {
+            Node<T1> head = null;
+            Node<T1> current = null;
+
+            foreach (var i in items )
+            {
+                
+                if (head == null)
+                {
+                    head = new Node<T1>() { Value = i };
+                    current = head;
+                }
+                else
+                {
+                    current.Next = new Node<T1>() { Value = i };
+                    current = current.Next;
+                }
+            }
+
+            return head;
+        }
+        #endregion
+    }
+}
