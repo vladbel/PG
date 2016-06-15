0ead6da9637673cfe4b5115e6d34a24211a51d54
Vladimir Beliaev <vladbel@live.com>
2016-06-15 17:06:31 -0500

    PG.BT.BuildBalancedTreeFromArray()

---
+++ PG.BT.Tests/BuildBalancedTreeFromArrayTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.BT.Tests
+{
+    [TestClass]
+    public class UnitTest1BuildBalancedTreeFromArrayTests
+    {
+        [TestMethod]
+        public void BuildFrom_EmptyArray()
+        {
+            var root = Tree.BuildBalancedTreeFromArray(new int[] {});
+            Assert.IsNull(root);
+        }
+
+        [TestMethod]
+        public void BuildFrom_Array_With_1_Element()
+        {
+            var root = Tree.BuildBalancedTreeFromArray(new int[] {3});
+            Assert.AreEqual(3, root.Value);
+            Assert.IsNull(root.Left);
+            Assert.IsNull(root.Right);
+        }
+
+        [TestMethod]
+        public void BuildFrom_Array_With_2_Element()
+        {
+            var root = Tree.BuildBalancedTreeFromArray(new int[] { 2, 3 });
+            Assert.AreEqual(2, root.Value);
+            Assert.IsNull(root.Left);
+            Assert.AreEqual(3, root.Right.Value);
+        }
+
+        [TestMethod]
+        public void BuildFrom_Array_With_3_Element()
+        {
+            var root = Tree.BuildBalancedTreeFromArray(new int[] { 1, 2, 3 });
+            Assert.AreEqual(2, root.Value);
+            Assert.AreEqual(1, root.Left.Value);
+            Assert.AreEqual(3, root.Right.Value);
+        }
+
+        [TestMethod]
+        public void BuildFrom_Array_With_4_Element()
+        {
+            var root = Tree.BuildBalancedTreeFromArray(new int[] { 1, 2, 3, 4 });
+            Assert.AreEqual(2, root.Value);
+            Assert.AreEqual(1, root.Left.Value);
+            Assert.AreEqual(3, root.Right.Value);
+            Assert.AreEqual(4, root.Right.Right.Value);
+            Assert.IsNull(root.Right.Left);
+        }
+
+
+    }
+}
--- PG.BT/Tree.cs
+++ PG.BT/Tree.cs
@@ -19,10 +19,65 @@ namespace PG.BT
                             root.Right == null ? depth : MaxDepth(root.Right, depth + 1));
         }
 
+        public static int MinDepth(TreeNode root, int depth = 0)
+        {
+            if (root == null)
+            {
+                throw new NullReferenceException("Tree root can't be null");
+            }
+            return Math.Min(root.Left == null ? depth : MinDepth(root.Left, depth + 1),
+                            root.Right == null ? depth : MinDepth(root.Right, depth + 1));
+        }
+
+        public static bool IsBalanced ( TreeNode root)
+        {
+            return MaxDepth(root) - MinDepth(root) <= 1;
+        }
+
+        public static bool IsBinarySearchTree(TreeNode root)
+        {
+            return root.Left == null ? true : root.Value > MaxValue(root.Left) && IsBinarySearchTree(root.Left)
+                && root.Right == null ? true : root.Value < MinValue(root.Right) && IsBinarySearchTree(root.Right);
+        }
+
         public static int MaxValue (TreeNode root)
         {
             return Math.Max(root.Left == null ? root.Value : Math.Max(root.Value,MaxValue(root.Left)),
                             root.Right == null ? root.Value : Math.Max(root.Value, MaxValue(root.Right)));
+        }
+
+        public static int MinValue(TreeNode root)
+        {
+            return Math.Min(root.Left == null ? root.Value : Math.Min(root.Value, MinValue(root.Left)),
+                            root.Right == null ? root.Value : Math.Min(root.Value, MinValue(root.Right)));
+        }
+
+        public static TreeNode BuildBalancedTreeFromArray (int[] array, int start = -1, int end = -1)
+        {
+            if (start == -1 && end == -1)
+            {
+                start = 0;
+                end = array.Length - 1;
+            }
+
+            if (start == end)
+            {
+                return new TreeNode() { Value = array[start] };
+            }
+            else if (end < start)
+            {
+                return null;
+            }
+            else
+            {
+                var mid = (start + end) / 2;
+                return new TreeNode()
+                {
+                    Value = array[mid],
+                    Left = BuildBalancedTreeFromArray(array, start, mid-1),
+                    Right = BuildBalancedTreeFromArray(array, mid+1, end)
+                };
+            }
         }
     }
 }
