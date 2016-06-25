9535ecf00fae91efc486d52ba1d6fc6fa5e4a27d
Vladimir Beliaev <vladbel@live.com>
2016-06-25 17:38:58 -0500

    PG.Search.LongestCommonSubstring

---
+++ PG.Search.Tests/LongestCommonSubstringTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.Search.Tests
+{
+    [TestClass]
+    public class LongestCommonSubstringTests
+    {
+        [TestMethod]
+        public void ShallFind_abc()
+        {
+            var results = LongestCommonSubstring.Find("1abc2", "3abc4");
+            Assert.AreEqual("abc", results[0]);
+        }
+
+        [TestMethod]
+        public void ShallFind_abc_02()
+        {
+            var results = LongestCommonSubstring.Find("12abc21", "34abc43");
+            Assert.AreEqual("abc", results[0]);
+        }
+
+        [TestMethod]
+        public void ShallFind_abc_AtTheBeginning()
+        {
+            var results = LongestCommonSubstring.Find("abc21", "12abc43");
+            Assert.AreEqual("abc", results[0]);
+
+            results = LongestCommonSubstring.Find("21abc21", "abc43");
+            Assert.AreEqual("abc", results[0]);
+
+            results = LongestCommonSubstring.Find("abc21", "abc43");
+            Assert.AreEqual("abc", results[0]);
+        }
+    }
+}
---
+++ PG.Search/LongestCommonSubstring.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.Search
+{
+    public class LongestCommonSubstring
+    {
+        public static List<string> Find(string s1, string s2)
+        {
+            var matches = new int[s1.Length, s2.Length];
+            int maxLength = 0;
+            var result = new List<string>();
+
+            for ( var i = 0; i < s1.Length - 1; i++)
+            {
+                for (var j = 0; j < s2.Length - 1; j++)
+                {
+                    if (s1[i] == s2[j])
+                    {
+                        matches[i, j] = (i > 0 && j > 0) ?
+                            matches[i - 1, j - 1] + 1 : 1;
+
+                        if (matches[i, j] > maxLength)
+                        {
+                            maxLength = matches[i, j];
+                            result = new List<string>();
+                        }
+
+                        if (matches[i, j] >= maxLength)
+                        {
+                            result.Add(s2.Substring(j - matches[i, j] + 1, matches[i, j]));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
