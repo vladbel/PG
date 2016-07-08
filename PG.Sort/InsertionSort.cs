5d3e602053679a2e4175f6c81e7083c994a4f6a3
Vladimir Beliaev <vladbel@live.com>
2016-07-08 18:14:38 -0500

    PG.Sort.InsertionSort

---
+++ PG.Sort/InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.Sort
+{
+    public static class InsertionSort
+    {
+        public static void Sort( int[] array)
+        {
+            if (array == null)
+            {
+                throw new NullReferenceException();
+            }
+            else if (array.Length < 2)
+            {
+                return;
+            }
+            for ( var i = 1; i < array.Length; i++)
+            {
+                var j = i;
+                while ( j > 0 && array[j-1] > array[j])
+                {
+                    var buffer = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = buffer;
+                    j--;
+                }
+            }
+        }
+    }
+}
