7705b7c05af87b6296945c394950e18d44b96098
Vladimir Beliaev <vladbel@live.com>
2016-07-13 20:59:55 -0500

    PG.BT.Heap (not completed)

---
+++ PG.BT.Tests/HeapTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.BT.Tests
+{
+    [TestClass]
+    public class HeapTests
+    {
+        [TestMethod]
+        public void ParentTests()
+        {
+            Assert.AreEqual(0, Heap.Parent(1));
+            Assert.AreEqual(0, Heap.Parent(2));
+            Assert.AreEqual(1, Heap.Parent(3));
+            Assert.AreEqual(1, Heap.Parent(4));
+            Assert.AreEqual(2, Heap.Parent(5));
+            Assert.AreEqual(2, Heap.Parent(6));
+            Assert.AreEqual(3, Heap.Parent(7));
+            Assert.AreEqual(3, Heap.Parent(8));
+            Assert.AreEqual(4, Heap.Parent(9));
+            Assert.AreEqual(4, Heap.Parent(10));
+        }
+
+        [TestMethod]
+        public void LeftTests()
+        {
+            Assert.AreEqual(1, Heap.Left(0));
+            Assert.AreEqual(3, Heap.Left(1));
+            Assert.AreEqual(5, Heap.Left(2));
+        }
+
+        [TestMethod]
+        public void RightTests()
+        {
+            Assert.AreEqual(2, Heap.Right(0));
+            Assert.AreEqual(4, Heap.Right(1));
+            Assert.AreEqual(6, Heap.Right(2));
+        }
+    }
+}
---
+++ PG.BT/Heap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.BT
+{
+    public class Heap
+    {
+        public static int Parent(int i)
+        {
+            return (i + 1) / 2 - 1; //floor
+        }
+
+        public static int Left (int i)
+        {
+            return 2 * (i + 1) - 1;
+        }
+
+        public static int Right (int i)
+        {
+            return 2 * (i + 1);
+        }
+
+        public static void SetMaxHipProperty( int[] array, int index)
+        {
+            var leftChild = Left(index);
+            var rightChild = Right(index);
+            int largest;
+
+            if (leftChild < array.Length && array[leftChild] > array[index])
+            {
+                largest = leftChild;
+            }
+            else
+            {
+                largest = index;
+            }
+
+            if (rightChild < array.Length && array[rightChild] > array[index])
+            {
+                largest = rightChild;
+            }
+            if (largest != index)
+            {
+                var a = array[index];
+                array[index] = array[largest];
+                array[largest] = a;
+                SetMaxHipProperty(array, largest);
+            }
+        }
+    }
+}
