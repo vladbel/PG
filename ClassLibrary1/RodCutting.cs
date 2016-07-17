b2e0838d3fa2d6bc3e944cedb758fec82198889d
Vladimir Beliaev <vladbel@live.com>
2016-07-17 18:27:27 -0500

    PG.DP.RodCutting.Cut_Recurcive()

---
+++ ClassLibrary1/RodCutting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class RodCutting
+    {
+        public static int Cut_Recurcive ( int length, int[] prices)
+        {
+            int maxRevenue = prices[length];
+
+            if (length == 0)
+            {
+                return prices[0];
+            }
+
+            else if (length == 1)
+            {
+                return prices[1];
+            }
+
+            for (var i = 1; i < length; i++)
+            {
+                maxRevenue = Math.Max(maxRevenue, 
+                    prices[i] + Cut_Recurcive(length - i, prices));
+            }
+
+            return maxRevenue;
+        }
+    }
+}
