2ab23bd9da72d23d9f4989124091ed557d4a1b4d
Vladimir Beliaev <vladbel@live.com>
2016-10-12 15:19:14 -0500

    PG.DP: longest subarray

---
+++ ClassLibrary1/LongestSubarray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.DP
+{
+    public class LongestSubarray
+    {
+        public static int LongestSubArrayThatAddsToTheGivenSum( int[] array, int sum)
+        {
+            var result = 0;
+            var subResults = new int[array.Length];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = 0; j<= i; j++)
+                {
+                    subResults[j] += array[i];
+
+                    if (subResults[j] == sum && (i - j + 1) > result)
+                    {
+                        result = i - j + 1;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PG.DP.Tests/LongestSubarrayWhichAddsToGivenSumTests.cs
@@ -0,0 +1,67 @@
+﻿using PG.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.DP.Tests
+{
+    [TestClass]
+    public class LongestSubarrayWhichAddsToGivenSumTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var array = new int[] { 1 };
+
+            var result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 1);
+            Assert.AreEqual(1, result);
+
+            result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 2);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test_2()
+        {
+            var array = new int[] { 1, 2};
+
+            var result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 1);
+            Assert.AreEqual(1, result);
+
+            result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 2);
+            Assert.AreEqual(1, result);
+
+            result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 3);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void Test_3()
+        {
+            var array = new int[] { 1, -1, 1};
+
+            var result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 1);
+            Assert.AreEqual(3, result);
+
+            result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 0);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Test_4()
+        {
+            var array = new int[] { 5, -1, 1, 0};
+
+            var result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 5);
+            Assert.AreEqual(4, result);
+
+            result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 4);
+            Assert.AreEqual(2, result);
+
+            result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 0);
+            Assert.AreEqual(3, result);
+
+            result = LongestSubarray.LongestSubArrayThatAddsToTheGivenSum(array, 1);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
