1956c1d41cc557a1ed16eb8088280445d54a2798
Vladimir Beliaev <vladbel@live.com>
2016-10-12 16:28:07 -0500

    PG.DP: repeated element count

---
+++ ClassLibrary1/RepeatingElementMaxCount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.DP
+{
+    public class RepeatingElementMaxCount
+    {
+        /// <summary>
+        /// assums all elements are 
+        /// 1) positive
+        /// 2) much less then int.MaxValue ( no overflow )
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static Tuple<int, int> Count(int[] array)
+        {
+            // Find element cieling - O(n)
+            int cieling = 0;
+            for (var i = 0; i < array.Length; i++)
+            {
+                if ( array[i] > cieling)
+                {
+                    cieling = array[i];
+                }
+            }
+            cieling += 1;
+
+            //cieling shall not be greater than array length
+
+            // count repetitions
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[array[i] % cieling] += cieling;
+            }
+
+
+            // read results O(n) - could be combined with prev step
+            int maxCount = 0;
+            int elementValue = 0;
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                var curCount = array[i] / cieling;
+                var curVal = i;
+
+                if ( curCount > maxCount)
+                {
+                    maxCount = curCount;
+                    elementValue = curVal;
+                }
+            }
+
+            // restore
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] % cieling;
+            }
+
+            return new Tuple<int, int>(elementValue, maxCount);
+        }
+    }
+}
---
+++ PG.DP.Tests/RepeatingElementMaxCountTests.cs
@@ -0,0 +1,42 @@
+﻿using PG.DP;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.DP.Tests
+{
+    [TestClass]
+    public class RepeatingElementMaxCountTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var array = new int[] { 1, 2, 1 };
+
+            var result = RepeatingElementMaxCount.Count(array);
+
+            Assert.AreEqual(1, result.Item1); // element with max count value
+            Assert.AreEqual(2, result.Item2); // count
+        }
+
+        [TestMethod]
+        public void Test_2()
+        {
+            var array = new int[] { 1, 2, 1, 2 };
+
+            var result = RepeatingElementMaxCount.Count(array);
+
+            Assert.AreEqual(1, result.Item1); // element with max count value
+            Assert.AreEqual(2, result.Item2); // count
+        }
+
+        [TestMethod]
+        public void Test_3()
+        {
+            var array = new int[] { 1, 2, 1, 2, 2 };
+
+            var result = RepeatingElementMaxCount.Count(array);
+
+            Assert.AreEqual(2, result.Item1); // element with max count value
+            Assert.AreEqual(3, result.Item2); // count
+        }
+    }
+}
