80ddc662d95b673a19ca974774e8a091ebdb8b0d
Vladimir Beliaev <vladbel@live.com>
2016-10-12 19:54:30 -0500

    PG.Console: ex 1

--- PG.Console/Program.cs
+++ PG.Console/Program.cs
@@ -10,8 +10,38 @@ namespace PG.Console
     {
         static void Main(string[] args)
         {
-           
-            
+            Ex_1();
+        }
+
+        /// <summary>
+        /// Find if array has element, so left sum = right sum
+        /// </summary>
+        static void Ex_1()
+        {
+            var numberOfTests = System.Console.ReadLine();
+            var arrayLength = System.Console.ReadLine();
+            var arrayString = System.Console.ReadLine();
+            var array = arrayString.Split(" "[0]).Select(elem => int.Parse(elem)).ToArray();
+
+            var rightSum = array.Sum();
+            var result = "NO";
+            var leftSum = 0;
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (leftSum == rightSum - array[i])
+                {
+                    result = "YES";
+                    break;
+                }
+                leftSum += array[i];
+                rightSum -= array[i];
+            }
+
+            System.Console.WriteLine(result);
+            System.Console.WriteLine(leftSum.ToString() + " X " + rightSum.ToString());
+
+            System.Console.ReadLine();
         }
     }
 }
