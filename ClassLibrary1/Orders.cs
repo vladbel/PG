0d189d1b439876745497b8bca132034de4d40619
Vladimir Beliaev <vladbel@live.com>
2016-10-16 22:30:58 -0500

    DP.Orders

---
+++ ClassLibrary1/Orders.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.DP
+{
+    // from Amazon interview
+    public class Orders
+    {
+        public static Dictionary<string, int> DepartmentsWithIsolatedCustomerCount ( List<Order> orders)
+        {
+            var departments = new Dictionary<string, int>();
+            var customers = new Dictionary<string, string>();
+            var notIsolated = "n/i";
+
+            foreach (Order order in orders)
+            {
+                if (!customers.ContainsKey(order.CustomrId))
+                {
+                    customers.Add(order.CustomrId, order.DepartmentId);
+                    // This is new costomer, potentially isolated
+                    if (departments.ContainsKey(order.DepartmentId))
+                    {
+                        departments[order.DepartmentId]++;
+                    }
+                    else
+                    {
+                        departments.Add(order.DepartmentId, 1);
+                    }
+                }
+                else if (customers[order.CustomrId] != order.DepartmentId
+                    && customers[order.CustomrId] != notIsolated)
+                {
+                    // this is not isolated customer
+                    departments[customers[order.CustomrId]]--;
+                    customers[order.CustomrId] = notIsolated;
+                }
+            }
+
+            return departments;
+        }
+    }
+
+    public class Order
+    {
+        public string CustomrId { get; set; }
+        public string DepartmentId { get; set; }
+    }
+}
---
+++ PG.DP.Tests/OrdersTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PG.DP.Tests
+{
+    [TestClass]
+    public class OrdersTest
+    {
+        [TestMethod]
+        public void DepartmentWithIsolatedCustomerCountTest_1()
+        {
+            var orders = new List<Order>() {
+                new Order() { CustomrId = "John", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "John", DepartmentId = "Food" },
+            };
+
+            var result = Orders.DepartmentsWithIsolatedCustomerCount(orders);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, result["Books"]);
+        }
+
+        [TestMethod]
+        public void DepartmentWithIsolatedCustomerCountTest_2()
+        {
+            var orders = new List<Order>() {
+                new Order() { CustomrId = "John", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "John", DepartmentId = "Food" },
+                new Order() { CustomrId = "Sam", DepartmentId = "Food" }
+            };
+
+            var result = Orders.DepartmentsWithIsolatedCustomerCount(orders);
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(1, result["Books"]);
+            Assert.AreEqual(1, result["Food"]);
+        }
+
+        [TestMethod]
+        public void DepartmentWithIsolatedCustomerCountTest_3()
+        {
+            var orders = new List<Order>() {
+                new Order() { CustomrId = "John", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "John", DepartmentId = "Food" },
+                new Order() { CustomrId = "Sam", DepartmentId = "Food" }
+            };
+
+            var result = Orders.DepartmentsWithIsolatedCustomerCount(orders);
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(1, result["Books"]);
+            Assert.AreEqual(1, result["Food"]);
+        }
+
+        [TestMethod]
+        public void DepartmentWithIsolatedCustomerCountTest_4()
+        {
+            var orders = new List<Order>() {
+                new Order() { CustomrId = "John", DepartmentId = "Books" },
+                new Order() { CustomrId = "John", DepartmentId = "Pets" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "John", DepartmentId = "Food" },
+                new Order() { CustomrId = "Sam", DepartmentId = "Food" },
+                new Order() { CustomrId = "Piter", DepartmentId = "Food" }
+            };
+
+            var result = Orders.DepartmentsWithIsolatedCustomerCount(orders);
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(1, result["Books"]);
+            Assert.AreEqual(2, result["Food"]);
+        }
+
+        [TestMethod]
+        public void DepartmentWithIsolatedCustomerCountTest_5()
+        {
+            var orders = new List<Order>() {
+                new Order() { CustomrId = "John", DepartmentId = "Books" },
+                new Order() { CustomrId = "John", DepartmentId = "Pets" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "Alice", DepartmentId = "Books" },
+                new Order() { CustomrId = "John", DepartmentId = "Food" },
+                new Order() { CustomrId = "Sam", DepartmentId = "Food" },
+                new Order() { CustomrId = "Piter", DepartmentId = "Food" },
+                new Order() { CustomrId = "Piter", DepartmentId = "Pets" }
+            };
+
+            var result = Orders.DepartmentsWithIsolatedCustomerCount(orders);
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(1, result["Books"]);
+            Assert.AreEqual(1, result["Food"]);
+        }
+    }
+}
