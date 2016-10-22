423465ff96d6cd0064efa923798ef59151f1e1f1
Vladimir Beliaev <vladbel@live.com>
2016-10-22 16:39:41 -0500

    Pg.Ex: matrix max conected region

--- PG.Ex/MatrixMaxConnectedRegion.cs
+++ PG.Ex/MatrixMaxConnectedRegion.cs
@@ -6,8 +6,54 @@ using System.Threading.Tasks;
 
 namespace PG.Ex
 {
+    /// <summary>
+    /// from: https://www.hackerrank.com/challenges/connected-cell-in-a-grid
+    /// Consider a matrix with  rows and  columns, where each cell contains either a 0 or a 0 and any
+    /// cell containing a 1 is called a filled cell. Two cells are said to be connected if they are adjacent 
+    /// to each other horizontally, vertically, or diagonally; and , provided that the location exists in the matrix for that .
+    ///
+    ///If one or more filled cells are also connected, they form a region.
+    ///Note that each cell in a region is connected to at least one other cell in the region but is not necessarily 
+    ///directly connected to all the other cells in the region.
+    ///
+    ///Task
+    ///Given an matrix, find and print the number of cells in the largest region in the matrix.
+    ///Note that there may be more than one region in the matrix.
+    /// </summary>
     public class MatrixMaxConnectedRegion
     {
+        public static int[,] ReadMatrix()
+        {
+            var rows = Int32.Parse(System.Console.ReadLine());
+            var columns = Int32.Parse(System.Console.ReadLine());
+            var matrix = new int[rows, columns];
+            for (var i = 0; i < rows; i++)
+            {
+                var row = System.Console.ReadLine().Split(' ').Select(e => Int32.Parse(e)).ToArray();
+                for (var j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void WriteMatrix(int[,] matrix)
+        {
+            for (var ri = 0; ri < matrix.GetLength(0); ri++)
+            {
+                var row = "";
+                var delimiter = "";
+                for (var ci = 0; ci < matrix.GetLength(1); ci++)
+                {
+                    row += delimiter + matrix[ri, ci].ToString();
+                    delimiter = " ";
+                }
+                System.Console.WriteLine(row);
+            }
+        }
+
         public static int ProcessMatrix( int[,] m)
         {
             const int IS_REGION = 1;
