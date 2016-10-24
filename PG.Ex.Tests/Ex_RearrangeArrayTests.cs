9ceb522c1b4a8b32d91fbc3f7292d2ad8aa8f83f
Vladimir Beliaev <vladbel@live.com>
2016-10-24 15:53:35 -0500

    PG.Ex: rearrange array

---
+++ PG.Ex.Tests/Ex_RearrangeArrayTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PG.Ex;
+
+namespace PG.Ex.Tests
+{
+    [TestClass]
+    public class Ex_RearrangeArrayTests
+    {
+
+        [TestMethod]
+        public void TestSwap_1()
+        {
+            var a = new int[] { 0, 1, 2, 3, 100, 101, 102, 103 };
+            RearrangeArray.Swap(1, a);
+
+            Assert.AreEqual(1, a[2]);
+
+        }
+
+
+    }
+}
---
+++ PG.Ex/RearrangeArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.Ex
+{
+    public class RearrangeArray
+    {
+        /// <summary>
+        /// rearrange array [0,1,2,3] to [0,2,1,3] - alternate items from bottom half and top half
+        /// /// </summary>
+        /// <param name="a"></param>
+        public static void ShuffleHalves(int[] a)
+        {
+            if (a.Length % 2 != 0)
+            {
+                throw new Exception("Array must have even length");
+            }
+
+        }
+
+        public static int NewIndex(int oldIndex, int[] a)
+        {
+            var ni = 0;
+            if (oldIndex < a.Length / 2)
+            {
+                ni = oldIndex * 2;
+            }
+            else
+            {
+                ni = (oldIndex - a.Length / 2) + 1;
+            }
+            return ni;
+        }
+
+        public static int Swap(int i, int[] a)
+        {
+            var curIndex = NewIndex(i, a);
+            int forwardedValue = a[i];
+            var result = 0;
+
+            while (curIndex != i)
+            {
+                result++;
+                var tmp = a[curIndex];
+                a[curIndex] = forwardedValue;
+                forwardedValue = tmp;
+                curIndex = NewIndex(curIndex, a);
+            }
+
+            a[curIndex] = forwardedValue;
+
+            return result;
+        }
+
+    }
+
+
+}
