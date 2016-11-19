516f034ea868f43d4dc107e4a8ef372195ed7c3c
Vladimir Beliaev <vladbel@live.com>
2016-11-19 17:14:40 -0600

    PG.Ex: Linq

---
+++ PG.Ex.Tests/LinqTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace PG.Ex.Tests
+{
+    [TestClass]
+    public class LinqTests
+    {
+        private class Order
+        {
+            public string CustomerId { get; set; }
+            public string DepartmentId { get; set; }
+        }
+
+        private List<Order> _orders;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _orders = new List<Order>()
+            {
+                new Order()
+                {
+                    CustomerId = "c1",
+                    DepartmentId = "d1"
+                },
+                new Order()
+                {
+                    CustomerId = "c2",
+                    DepartmentId = "d1"
+                },
+                new Order()
+                {
+                    CustomerId = "c2",
+                    DepartmentId = "d2"
+                },
+                new Order()
+                {
+                    CustomerId = "c2",
+                    DepartmentId = "d3"
+                }
+
+            };
+        }
+
+        [TestMethod]
+        public void GetSpecificCustomer()
+        {
+            var order = _orders.Where(o => o.CustomerId == "c1").FirstOrDefault();
+            Assert.AreEqual("c1", order.CustomerId);
+        }
+    }
+}
