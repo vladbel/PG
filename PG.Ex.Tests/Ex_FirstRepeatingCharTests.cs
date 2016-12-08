1629e13292f725270516fa85f759c226af92a1c6
Vladimir Beliaev <vladbel@live.com>
2016-12-08 16:33:03 -0600

    PG.Ex: find first repeating char.

---
+++ PG.Ex.Tests/Ex_FirstRepeatingCharTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PG.Ex.Tests
+{
+    [TestClass]
+    public class Ex_FirstRepeatingCharTests
+    {
+        [TestMethod]
+        public void Test_1()
+        {
+            var str = "abccb";
+            var result = FirstRepeatingChar.GetFirstChar(str);
+            Assert.AreEqual('b', result);
+        }
+    }
+}
---
+++ PG.Ex/FirstRepeatingChar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.Ex
+{
+    public class FirstRepeatingChar
+    {
+        public static char GetFirstChar(string str)
+        {
+            if (str == null || str.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            var flags = Enumerable.Repeat(-1, 256).ToArray();
+
+
+            int firstPosition = int.MaxValue;
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (flags[(int)str[i]] >= 0)
+                {
+                    if (flags[(int)str[i]] < firstPosition) // A
+                    {
+                        firstPosition = i; // 2[C} -> 1[B]
+                    }
+                }
+                else
+                {
+                    flags[(int)str[i]] = i; // [ .....0[A],1[B],2[C].....]
+                }
+            }
+
+            return str[firstPosition]; // ??
+
+        }
+    }
+}
