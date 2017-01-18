6749c61c7d943ae0dfb6e8d43536f1430e4ffe6d
Vladimir Beliaev <vladbel@live.com>
2017-01-18 17:01:46 -0600

    Sort: Bubble sort

---
+++ PG.Sort/BubbleSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG.Sort
+{
+    public class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            var start = 0;
+            var end = array.Length - 1;
+
+            while (start < end)
+            {
+                for (var i = start; i < end; i++)
+                {
+                    if (array[i] > array[i+1])
+                    {
+                        // swap values
+                        array[i+1] += array[i];
+                        array[i] = array[i + 1] - array[i];
+                        array[i + 1] -= array[i];
+                    }
+                }
+                end--;
+
+                for (var i = end; i > start; i--)
+                {
+                    if (array[i] < array[i - 1])
+                    {
+                        // swap values
+                        array[i - 1] += array[i];
+                        array[i] = array[i - 1] - array[i];
+                        array[i - 1] -= array[i];
+                    }
+                }
+                start ++;
+            }
+        }
+    }
+}
